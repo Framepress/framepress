3ccc50ada023ae21913961072be5b73a4809b643
Vladymyr Protsenko <grayfolk@gmail.com>
2018-02-08 16:08:29 +0200

    Remove singleton

---
+++ src/Application.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Framepress;
+
+defined ( 'DS' ) or define ( 'DS', DIRECTORY_SEPARATOR );
+
+use Framepress\Framepress;
+use Framepress\controllers\Common;
+use Framepress\controllers\Front;
+use Framepress\controllers\Admin;
+
+class Application {
+	private $container;
+	public function __construct(array $config = []) {
+		Framepress::$app = $this;
+		$this->parseConfig ( $config );
+		$this->container = new \League\Container\Container ();
+		$this->container->add ( 'view', new \League\Plates\Engine ( implode ( DS, [ 
+				Framepress::$appPath,
+				'views' 
+		] ) ) );
+		new Common ();
+		if (! is_admin ()) {
+			new Front ();
+		} else {
+			new Admin ();
+		}
+	}
+	public function __get($var) {
+		if (self::getInstance ()->container->has ( $var ))
+			return self::getInstance ()->container->get ( $var );
+	}
+	/**
+	 *
+	 * @param array $config        	
+	 */
+	private function parseConfig(array $config) {
+		Framepress::$path = __DIR__;
+		Framepress::$config ['appPath'] = dirname ( dirname ( dirname ( dirname ( __DIR__ ) ) ) );
+		Framepress::$config = array_merge ( Framepress::$config, $config );
+		if (! empty ( $config ['id'] ))
+			Framepress::$id = $config ['id'];
+		if (! empty ( $config ['appName'] ))
+			Framepress::$appName = $config ['appName'];
+		if (! empty ( $config ['appPath'] ))
+			Framepress::$appPath = $config ['appPath'];
+	}
+}
--- src/Framepress.php
+++ src/Framepress.php
@@ -2,12 +2,6 @@
 
 namespace Framepress;
 
-defined ( 'DS' ) or define ( 'DS', DIRECTORY_SEPARATOR );
-
-use Framepress\controllers\Common;
-use Framepress\controllers\Front;
-use Framepress\controllers\Admin;
-
 /**
  *
  * @author grayfolk
@@ -65,42 +59,4 @@ class Framepress {
 			],
 			'cli' => [ ] 
 	];
-	/**
-	 *
-	 * @param array $config        	
-	 */
-	function __construct(array $config = []) {
-		$this->parseConfig ( $config );
-		self::$app = \Framepress\base\Application::init ();
-		new Common ();
-		if (! is_admin ()) {
-			new Front ();
-		} else {
-			new Admin ();
-		}
-	}
-	/**
-	 *
-	 * @param string $var        	
-	 * @return \Framepress\unknown
-	 */
-	public function __get($var) {
-		if (! empty ( self::$config [$var] ))
-			return self::$config [$var];
-	}
-	/**
-	 *
-	 * @param array $config        	
-	 */
-	private function parseConfig(array $config) {
-		self::$path = __DIR__;
-		self::$config ['appPath'] = dirname ( dirname ( dirname ( dirname ( __DIR__ ) ) ) );
-		self::$config = array_merge ( self::$config, $config );
-		if (! empty ( $config ['id'] ))
-			self::$id = $config ['id'];
-		if (! empty ( $config ['appName'] ))
-			self::$appName = $config ['appName'];
-		if (! empty ( $config ['appPath'] ))
-			self::$appPath = $config ['appPath'];
-	}
 }
